09c309da7ead03596dd934335297dd8a475b647a
ashishmashal <ashishmashal5@gmail.com>
2022-05-12 18:45:24 +0530

    Added: Validation For Email Is Added.

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -1,10 +1,39 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class UserRegistrationMain {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Problem");
         System.out.println("----------------------------------------");
-        UserRegistrationOprations.validFirstName();
-        UserRegistrationOprations.validLastName();
+        int ans;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter Your Choice " +
+                    "\n1.Validate First Name  " +
+                    "\n2.Validate Last Name " +
+                    "\n3.Validate Email "
+                    );
+
+            int Option = scanner.nextInt();
+            switch (Option) {
+                case 1:
+                    UserRegistrationOprations.validFirstName();
+                    break;
+                case 2:
+                    UserRegistrationOprations.validLastName();
+                    break;
+                case 3 :
+                    UserRegistrationOprations.validEmail();
+                    break;
+                default:
+                    System.out.println("Enter Valid No");
+                    break;
+            }
+            System.out.println("If Continue Press 1 ");
+            ans = scanner.nextInt();
+        }while(ans == 1);
+
+
     }
 }
--- src/main/java/com/bridgelabz/UserRegistrationOprations.java
+++ src/main/java/com/bridgelabz/UserRegistrationOprations.java
@@ -5,17 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationOprations {
-    public static final Scanner sc = new Scanner(System.in);
-
+    public static final Scanner scanner = new Scanner(System.in);
+    static Pattern patternObject;
+    static Matcher matcherObject;
+    static boolean Return;
     /*
     Method For Validation for Last Name
      */
     public static boolean validFirstName() {
         System.out.print("Enter the First Name : ");
-        String name = sc.next();
+        String name = scanner.next();
         String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern patternObject = Pattern.compile(regex);
-        Matcher matcherObject = patternObject.matcher(name);
+         patternObject = Pattern.compile(regex);
+         matcherObject = patternObject.matcher(name);
         boolean Return = matcherObject.matches();
 
         if (Return)
@@ -29,16 +31,34 @@ public class UserRegistrationOprations {
      */
     public static boolean validLastName() {
         System.out.print("Enter the Last Name : ");
-        String name = sc.next();
+        String name = scanner.next();
         String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean r = m.matches();
+        patternObject = Pattern.compile(regex);
+        matcherObject = patternObject.matcher(name);
+        Return = matcherObject.matches();
 
-        if (r)
+        if (Return)
             System.out.println("Last name is valid.");
         else
             System.out.println("Last name is invalid.");
-        return r;
+        return Return;
+    }
+    /*
+    Method For Validation for Email
+     */
+    public static boolean validEmail()
+    {
+        System.out.print("Enter the Email : ");
+        String email = scanner.next();
+        String regex = "^[a-z0-9]+@[a-z0-9]+.[a-z]{2,3}+.[a-z]{2,}$";
+        patternObject= Pattern.compile(regex);
+        matcherObject= patternObject.matcher(email);
+        Return = matcherObject.matches();
+
+        if (Return)
+            System.out.println("Email is valid.");
+        else
+            System.out.println("Email is invalid.");
+        return Return;
     }
 }
